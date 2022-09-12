69f6720c21259ad401147fbbecfe531323caa40f
Deysha Williams <deyshawilliams95@gmail.com>
2022-09-11 21:08:06 -0700

    Fixed error code.

--- Cars/views.py
+++ Cars/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view('GET')
+@api_view(['GET'])
 def cars_list(request):
-
+    pass
 
     return Response('ok')
